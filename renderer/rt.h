2015828bd4c2af55a5518c116f8fc5ab7e59e053
Abhigyan Bhusal <bhusalabhigyan@gmail.com>
2023-03-08 22:53:51 +0545

    Fix slow rng

--- renderer/rt.h
+++ renderer/rt.h
@@ -15,16 +15,15 @@ inline double degree_to_radian(double degree){
     return degree * PI / 180.0;
 }
 
-inline double random_double() {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
-    return distribution(generator);
-}
 
 inline double random_double(double min, double max) {
     static std::uniform_real_distribution<double> distribution(min, max);
     static std::mt19937 generator;
     return distribution(generator);
+}
+
+inline double random_double() {
+    return random_double(0.0, 1.0);
 }
 
 inline int random_int(int min, int max) {
